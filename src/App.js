19532d25c677a4de75553a6204f3f1e8506ddc8e
Ayman-tron <ayman.abdulkadir@ontariotechu.net>
2022-08-06 08:47:54 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (e) => {
+    e.preventDefault(); // This prevents a REFRESH
+    setTodos([...todos, input]);
+    setInput("")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+    <h1>Hello World</h1>
+    <form>
+      <input value = {input} 
+      onChange = {event => setInput(event.target.value)} 
+      type = "text"/>
+      <button type = "submit" onClick={addTodo}>Add to do</button>
+      </form>
+    <h2>List of Todos</h2>
+    {todos.map((todo) => (
+    <p>{todo}</p>))}
     </div>
   );
 }
